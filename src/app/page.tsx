e8cfeb1476680750b6caa3848d94628035989a7e
ecryptoguru <ecryptoguru@gmail.com>
2025-10-02 18:29:29 +0530

    update1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import {Button } from "@/components/ui/button";
+import {prisma } from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany()
   return ( 
     <div>
-      <Button variant="destructive">
-        Click me
-      </Button>
+      {JSON.stringify(users, null ,2)}
     </div>
   );
 }
